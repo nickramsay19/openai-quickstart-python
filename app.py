fb82a1f1fd45b9be6eff16802d4a8a82a7e2b44d
nickramsay19 <nickramsay19@gmail.com>
2023-04-26 09:40:52 +1000

    added flask sessions, changed interface

--- app.py
+++ app.py
@@ -1,35 +1,53 @@
 import os
-
+from urllib.request import urlopen
+import secrets
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# for session storage flask needs a key
+app.secret_key = secrets.token_bytes(64)
+#app.config['SESSION_TYPE'] = ''#'memcached'
 
-@app.route("/", methods=("GET", "POST"))
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+    if not session.permanent:
+        session.permanent = True
+    if session.new or 'messages' not in session.keys():
+        session['messages'] = []
+        session["temperature"] = 1.0
+    
+    if request.method == 'POST':
+        #session['response-type'] = request.form['response-type']
+        session['model'] = request.form['model']
+        
+        #print(session['temperature'], float(request.form['temperature']))
+        session['temperature'] = float(request.form['temperature'])
+        
+        session['messages'] += [{
+            'role': 'user',
+            'content': request.form['prompt'],
+        }]
+        
+        response = openai.ChatCompletion.create(
+            model=session["model"],
+            temperature=session['temperature'],
+            messages=session["messages"],
+            max_tokens=500
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        
+        response_content = response['choices'][0]['message']['content']
+        
+        session['messages'] += [{
+            'role': 'assistant',
+            'content': response_content
+        }]
+        
+    return render_template('index.html', session=session)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+@app.route('/image', methods=('GET', 'POST'))
+def image():
+    return ''
